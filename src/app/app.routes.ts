204dcafa28fe60bd333d209047944c9137eff34e
Muhammad Shamim <shaminaus@gmail.com>
2025-08-05 17:30:55 -0500

    Added Bootstrap and Font-Awesome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TeamComponent } from './team.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'team', component: TeamComponent, loadComponent: () => import('./team.component').then(m => m.TeamComponent) },
+];
