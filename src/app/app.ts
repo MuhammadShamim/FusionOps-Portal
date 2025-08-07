4e5f778c1ef440aae92b8641ab8298a6c785a959
Akhtar Shamim <akhtar.shamim@wwex.com>
2025-08-07 18:15:33 -0500

    ui chaneg 7

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { RouterModule, RouterOutlet, Router } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
-import { AuthService } from './auth.service';
+import { AuthService } from './services/auth.service';
 import { Subscription } from 'rxjs';
 
 @Component({
--- src/app/services/api-status.service.ts
+++ src/app/services/api-status.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { EnvService } from '../env.service';
+import { EnvService } from './env.service';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
