1ec7dc86ba9d15aa8f0e5b2765ca6eda56eb2779
Akhtar Shamim <akhtar.shamim@wwex.com>
2025-08-07 17:55:58 -0500

    removed monaco file

--- src/app/private/carrier-profile.monaco.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { MonacoEditorModule } from 'ngx-monaco-editor';
-
-@NgModule({
-  imports: [MonacoEditorModule.forRoot()],
-  exports: [MonacoEditorModule]
-})
-export class CarrierProfileMonacoModule {}
