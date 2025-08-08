0deecf4940792d454989cd9204d6566289066882
Akhtar Shamim <akhtar.shamim@wwex.com>
2025-08-08 00:18:40 -0500

    fix 8.14

--- src/app/public/public-layout.component.ts
+++ src/app/public/public-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'fusionops-public-layout',
   standalone: true,
   templateUrl: './public-layout.component.html',
   styleUrls: ['./public-layout.component.css'],
-  imports: [RouterModule, RouterLink, RouterLinkActive]
+  imports: [RouterModule]
 })
 export class PublicLayoutComponent {
   year = new Date().getFullYear();
